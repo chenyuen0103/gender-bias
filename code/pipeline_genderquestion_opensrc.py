1a10fc7b83fa165e11f56c4cebc5072376e18336
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-06-05 11:56:32 -0700

    add breakpoint

--- code/pipeline_genderquestion_opensrc.py
+++ code/pipeline_genderquestion_opensrc.py
@@ -162,9 +162,10 @@ def main(args):
                     # mean_log_prob = log_probs_of_interest.mean()
                     # total_prob = torch.exp(mean_log_prob).item()
 
-                    # if gender == 'diverse' and job == 'skincare specialist':
-                    #     top_k_tokens = get_top_k(model, tokenizer, prompt_text, top_k=10)
-                    #     print(top_k_tokens)
+                    if job == 'vet':
+                        top_k_tokens = get_top_k(model, tokenizer, prompt_text, top_k=10)
+                        breakpoint()
+                        print(top_k_tokens)
 
 
 
